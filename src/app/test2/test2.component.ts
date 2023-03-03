36308c4f5ec47d5678105511144b40e3e638b786
sahil <sahil@gmail.com>
2023-03-03 20:00:56 +0530

    ...

--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -13,28 +13,39 @@ export class Test2Component {
 
   program_list: Programs[] = [];
   project_list: Projects[] = [];
+  resultArray: any[] = [];
   @Input()
-  checkselected : any;
+  checkselected: any = [];
 
   constructor(private api: ApiserviceService) { }
 
   getProgram_list() {
     this.api.getProgram().subscribe(gram_res => { console.log(gram_res); this.program_list = gram_res; });
+    this.api.getProjects().subscribe(pro_res => { console.log(pro_res);  this.project_list = pro_res; });
+ 
   }
-  getProject_list(){
-    // this.api.getProjects().subscribe(pro_res => { console.log(pro_res);  this.project_list = pro_res; });
+  getProject_list() {
     console.log(this.checkselected);
-    
+
   }
-  checkProgramSelected(id:string){
-          for(let i in this.project_list){
-            if(id === this.api.projects[i].projectId) 
-            {
-              this.checkselected = this.api.projects[i].projectName;
-              console.log((this.checkselected));
-              
-            }
-          }
+  checkProgramSelected(id: string) {
+    this.checkselected.push(id)
+    console.warn('id ', id);
+    console.warn('ara', this.checkselected);
+    // console.warn(this.project_list);
+    
+    
+    this.project_list.forEach((data) => {
+      this.checkselected.forEach((res: any) => {
+        if (data.programId == res && this.resultArray
+          .findIndex(ap => ap.projectName == data.projectName) == -1) {
+          this.resultArray.push(data); 
+        }
+      })
+    })
+    console.warn('mit',this.resultArray);
+
+    
   }
   fetchdata() {
     this.api.getProgram().subscribe(gram_res => { console.log(gram_res); });
